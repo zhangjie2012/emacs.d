705dec08ea0c7aab0c8f2d2ca303d08838900fbb
JerryZhang <me@zhangjiee.com>
2022-09-08 11:26:13 +0800

    feat: python save hook

--- core/init-ide-python.el
+++ core/init-ide-python.el
@@ -6,6 +6,13 @@
   (setq python-shell-interpreter "python3")
   (set-variable 'py-indent-offset 4)
   (set-variable 'python-indent-guess-indent-offset nil)
+  :config
+  (defun lsp-py-install-save-hooks ()
+	(add-hook 'before-save-hook #'flycheck-buffer)
+	(add-hook 'before-save-hook #'lsp-organize-imports t t)
+	(add-hook 'before-save-hook #'lsp-format-buffer t t)
+	)
+  (add-hook 'python-mode-hook #'lsp-py-install-save-hooks)
   )
 
 (provide 'init-ide-python)
