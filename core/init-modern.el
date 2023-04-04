91be3f15caf2b1abef30ebf929413966e3f0ae81
JerryZhang <me@zhangjiee.com>
2023-04-04 10:16:24 +0800

    feat: format

--- core/init-modern.el
+++ core/init-modern.el
@@ -90,10 +90,8 @@
 
 (use-package youdao-dictionary
   :ensure t
-  :bind (
-	     ("<f9> f" . youdao-dictionary-search-at-point+)
-	     ("<f9> F" . youdao-dictionary-search-from-input)
-	     )
+  :bind (("<f9> f" . youdao-dictionary-search-at-point+)
+	     ("<f9> F" . youdao-dictionary-search-from-input))
   :config
   (setq url-automatic-caching t)
   (setq youdao-dictionary-use-chinese-word-segmentation t))
