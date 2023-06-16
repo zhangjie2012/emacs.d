083d18037deab15fb4992571ba9a2eab85ad6c90
JerryZhang <me@zhangjiee.com>
2023-06-16 09:59:09 +0800

    web-mode include json file

--- core/init-lang.el
+++ core/init-lang.el
@@ -112,6 +112,7 @@
   (add-to-list 'auto-mode-alist '("\\.xml?\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.css?\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.json\\'" . web-mode))
   (defun my-web-mode-hook ()
     "Hooks for Web mode."
     (setq-default indent-tabs-mode nil)
