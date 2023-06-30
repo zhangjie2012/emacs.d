699396bba111daaa891690ee67636bd772a83334
JerryZhang <me@zhangjiee.com>
2023-06-30 09:57:56 +0800

    remove org-appear, 会导致 org 中中英文表格不对齐

--- core/init-document.el
+++ core/init-document.el
@@ -26,7 +26,7 @@
 		org-log-done t
 		org-src-tab-acts-natively nil
 		org-pretty-entities t
-		org-hide-emphasis-markers t
+		org-hide-emphasis-markers nil
 		org-startup-folded t
 		org-startup-with-inline-images t
 		org-image-actual-width '(1024)
@@ -121,10 +121,6 @@
 	:ensure t
 	:init
 	(add-hook 'org-mode-hook 'toc-org-mode))
-
-  (use-package org-appear
-    :ensure t
-    :hook (org-mode . org-appear-mode))
   )
 
 ;; blog
