5c577657cd791932546f7652144f3088cbd4a34c
JerryZhang <me@zhangjiee.com>
2025-02-05 10:47:40 +0800

    remove key bind

--- core/init-keymap.el
+++ core/init-keymap.el
@@ -64,10 +64,6 @@ Uses `current-date-time-format' for the formatting the date/time."
     ("x" toggle-frame-fullscreen "toggle fullscreen" :exit t)
     ("X" toggle-frame-alpha "toggle alpha" :exit t)
 
-    ("<SPC>1" (find-file "~/gtd/entry.org") "GTD entry.org" :exit t :column "4. open")
-    ("<SPC>2" (find-file "~/.emacs.d/init.el") "emacs init.el" :exit t)
-    ("<SPC>3" (find-file "~/personal-area/self-growth/summary.org") "summary.org" :exit t)
-
     ("ti" display-fill-column-indicator-mode "toggle column indicator" :column "x. other")
     ("m" consult-imenu "imenu" :exit t)
     ("M" consult-outline "consult outline" :exit t)
