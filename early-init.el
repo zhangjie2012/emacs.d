fc82542fc2e616d7b7d41479c901d409d6858e52
JerryZhang <me@zhangjiee.com>
2026-02-13 17:31:36 +0800

    性能优化

--- early-init.el
+++ early-init.el
@@ -1,9 +1,11 @@
 (setq gc-cons-threshold (* 1024 1024 100) ;; 100m
       read-process-output-max (* 4 1024 1024)
+      process-adaptive-read-buffering nil
       gc-cons-percentage 0.6)
 
 (setq package-enable-at-startup nil)
 
+;; 临时禁用 file-name-handler-alist 以加速启动
 (defvar my--file-name-handler-alist file-name-handler-alist)
 (setq file-name-handler-alist nil)
 
@@ -18,11 +20,13 @@
 (setq default-frame-alist
       '((menu-bar-lines . 0)
         (tool-bar-lines . 0)
-        (horizontal-scroll-bars)
-        (vertical-scroll-bars)))
+        (vertical-scroll-bars . nil)
+        (horizontal-scroll-bars . nil)
+        (fullscreen . maximized)
+        (undecorated . t)))
 
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
-(add-to-list 'default-frame-alist '(undecorated . t))
+(when (fboundp 'pixel-scroll-precision-mode)
+  (pixel-scroll-precision-mode 1))
 
 (setq frame-inhibit-implied-resize t)
 
